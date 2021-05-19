21c5a8bb3740f48869503d67471ce039fb11ad74
agnieszka.tatarzyn <agnieszka.tatarzyn@rewardgateway.com>
2021-05-19 21:41:32 +0100

    added shop page and collection components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
     </div>
   );
 }
