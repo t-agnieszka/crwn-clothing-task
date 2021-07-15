6ea19285f40a7b22e9e0f590b4f0c9910b054d04
agnieszka.tatarzyn <agnieszka.tatarzyn@rewardgateway.com>
2021-07-15 18:36:56 +0100

    collection preview work, shop data and directory reducer

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+const selectShopData = (state) => state.shop;
+
+export const selectShopDataCollections = createSelector(
+  [selectShopData],
+  (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopDataCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectShopDataCollections],
+    (collections) => collections[collectionUrlParam]
+  );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import RootReducer from './root-reducer';
+import RootReducer from './root.reducer';
 
 const middlewares = [logger];
-const store = createStore(RootReducer, applyMiddleware(...middlewares));
+export const store = createStore(RootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
